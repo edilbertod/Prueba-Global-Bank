55ab97b623d202169c844bf01d6362b9881e8db7
Edilberto Duncan <essie0496ed@gmail.com>
2026-03-31 19:02:04 -0500

    Version 1.0

--- src/Palindromo.java
+++ src/Palindromo.java
@@ -6,17 +6,20 @@ public class Palindromo {
         String texto1 = new Scanner(System.in).nextLine();
         String texto2 = "";
         String texto3 = "";
+        //Quitar los espacios del texto
         for (int i=0; i<texto1.length(); i++){
             if (texto1.charAt(i) != ' '){
                 texto2 = texto2 + texto1.charAt(i);
-                System.out.println(texto2);
             }
         }
-
-        for (int j=texto1.length();j<1; j--){
+        int k = texto1.length();
+        System.out.println(k);
+        for (int j=texto1.length(); j>=1; j=j-1){
             if (texto1.charAt(j) != ' '){
                 texto3 = texto3 + texto1.charAt(j);
+                System.out.println(texto3);
             }
+            k=k-1;
         }
 
         if (texto2.equals(texto3)){
